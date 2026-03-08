507e6d9f50a52f44005aed6ea9f54bb907af0448
Jems <jems.maranion13@.com>
2026-03-08 20:37:42 +0800

    adjusted find_merge_materials

--- item_scrape.py
+++ item_scrape.py
@@ -3,27 +3,38 @@ import requests
 import re
 
 # URL for AQW Wiki
-url = "http://aqwwiki.wikidot.com/" 
+url = "http://aqwwiki.wikidot.com" 
+
+merge_items = []
+merge_links = []
+merge_li = None
 
 print("Input item:")
 search_item = input()
 search_item = search_item.replace(" ", "-")
 
-# for abbreviations (sample)
-if search_item.lower() == "nsod":
-    # necrotic sword of doom sword
-    search_item = "necrotic-sword-of-doom-sword"
-
-print(f'Main Page: {url}{search_item}')
-
-# html parse
 result = requests.get(f'{url}/{search_item}')
 doc = soup(result.text, "html.parser")
 
-# finder
-merge_items = []
-merge_links = []
-merge_li = None
+def check_item_exists():
+    for p in doc.find_all('strong'):
+        if "This page doesn't exist yet!" in p.get_text():
+            print("Item does not exist.")
+            return False
+    return True
+
+def check_item():
+
+    if check_item_exists():
+        print(f'Main Page: {url}/{search_item}')
+
+    check_merge()
+
+# for abbreviations (sample)
+# if search_item.lower() == "nsod":
+#     # necrotic sword of doom sword
+#     search_item = "necrotic-sword-of-doom-sword"
+
 
 def check_merge():
     for li in doc.find_all('li'):
@@ -40,27 +51,26 @@ def add_to_merge_list(items):
         
         merge_items.append(f'{item_li.get_text(strip=True)}')
 
-# material finder
-def find_materials(li):
+
+def find_merge_materials():
 
     merge_li = check_merge()
 
     if merge_li:
         
-        parent_ul = merge_li.find_parent('ul')
-        all_items = parent_ul.find_all('li')
+        all_items = merge_li.find_parent('ul').find_all('li')
         add_to_merge_list(all_items)
-        
-    return print("Cannot be merged.")
+    else:
+        return print("Cannot be merged.")
 
-
+#fix
 def parse_item(line):
     line = line.strip()
     
     # matches pattern of itemname + x + digits at the end
-    match = re.search(r'\s*x\s*(\d+)\s*$', line, re.IGNORECASE)
+    match = re.search(r'\s*x\s*(\d{1,3}(?:,\d{3})*)\s*$', line, re.IGNORECASE)
     if match:
-        qty = int(match.group(1))
+        qty = int(match.group(1).replace(',', ''))  # Remove commas before converting to int
         # removes 'x' and separates the qty
         item_name = re.sub(r'\s*x\s*\d+\s*$', '', line, flags=re.IGNORECASE)
         return {'name': item_name.strip(), 'qty': qty}
@@ -74,7 +84,9 @@ def parse_item(line):
     # No quantity found
     return {'name': line, 'qty': None}
 
-find_materials(merge_li)
+
+check_item() 
+find_merge_materials()
 
 parsed = [parse_item(item) for item in merge_items]
 
